16dc638ed67cbf193ea3987de77425389e73a26d
joao <joao.vr.fernandes@gmail.com>
2026-04-16 16:15:48 -0300

    Refatoração de ambiente

--- Front-end/app/vite.config.ts
+++ Front-end/app/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    // Add this if you are using a sub-path or specific proxying
+    hmr: {
+        clientPort: 80 
+    }
+  }
 })
